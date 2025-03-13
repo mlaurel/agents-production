1a22ef91bdc3d151dcd8b0cc352aadd027a1e59a
Mike Laurel <mlaurel@hey.com>
2025-03-12 17:05:29 -0700

    Adds Human in the Loop HITL approvals with structured output

--- src/agent.ts
+++ src/agent.ts
@@ -1,39 +1,82 @@
+import type { AIMessage } from "../types"
 import { addMessages, getMessages, saveToolResponse } from './memory'
-import { runLLM } from './llm'
+import { runApprovalCheck, runLLM } from './llm'
 import { showLoader, logMessage } from './ui'
 import { runTool } from './toolRunner'
+import { generateImageToolDefinition } from './tools/generateImage'
+
+const handleImageApprovalFlow = async (
+    history: AIMessage[],
+    userMessage: string
+) => {
+    // const lastMessage = history[history.length - 1]
+    const lastMessage = history.at(-1)
+    const toolCall = lastMessage?.tool_calls?.[0]
+
+    if (!toolCall || toolCall.function.name !== generateImageToolDefinition.name) {
+        return false
+    }
+
+    const loader = showLoader('Processing approval...')
+    const approved = await runApprovalCheck(userMessage)
+
+    if (approved) {
+        loader.update(`executing tool: ${toolCall.function.name}`)
+        const toolResponse = await runTool(toolCall, userMessage)
+
+        loader.update(`done: ${toolCall.function.name}`)
+        await saveToolResponse(toolCall.id, toolResponse)
+    } else {
+        await saveToolResponse(
+            toolCall.id,
+            'User did not approve image generation at this time.'
+        )
+    }
+
+    loader.stop()
+
+    return true
+}
 
 export const runAgent = async ({
-  userMessage,
-  tools,
+    userMessage,
+    tools,
 }: {
-  userMessage: string
-  tools: any[]
+    userMessage: string
+    tools: any[]
 }) => {
-  await addMessages([{ role: 'user', content: userMessage }])
-
-  const loader = showLoader('🤔')
-
-  while (true) {
     const history = await getMessages()
-    const response = await runLLM({ messages: history, tools })
+    const isApproval = await handleImageApprovalFlow(history, userMessage)
+    await addMessages([{ role: 'user', content: userMessage }])
 
-    await addMessages([response])
+    const loader = showLoader('🤔')
 
-    if (response.content) {
-      loader.stop()
-      logMessage(response)
-      return getMessages()
+    while (true) {
+        const history = await getMessages()
+        const response = await runLLM({ messages: history, tools })
+
+        await addMessages([response])
+
+        if (response.content) {
+            loader.stop()
+            logMessage(response)
+            return getMessages()
+        }
+
+        if (response.tool_calls) {
+            const toolCall = response.tool_calls[0]
+            logMessage(response)
+            loader.update(`executing: ${toolCall.function.name}`)
+
+            if (toolCall.function.name === generateImageToolDefinition.name) {
+                loader.update('need user approval')
+                loader.stop()
+                return getMessages()
+            }
+
+            const toolResponse = await runTool(toolCall, userMessage)
+            await saveToolResponse(toolCall.id, toolResponse)
+            loader.update(`done: ${toolCall.function.name}`)
+        }
     }
-
-    if (response.tool_calls) {
-      const toolCall = response.tool_calls[0]
-      logMessage(response)
-      loader.update(`executing: ${toolCall.function.name}`)
-
-      const toolResponse = await runTool(toolCall, userMessage)
-      await saveToolResponse(toolCall.id, toolResponse)
-      loader.update(`done: ${toolCall.function.name}`)
-    }
-  }
 }
--- src/llm.ts
+++ src/llm.ts
@@ -1,37 +1,57 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
-import { zodFunction } from 'openai/helpers/zod'
+import { zodFunction, zodResponseFormat } from 'openai/helpers/zod'
 import { systemPrompt as defaultSystemPrompt } from './systemPrompt'
+import { z } from 'zod'
 
 export const runLLM = async ({
-  messages,
-  tools = [],
-  temperature = 0.1,
-  systemPrompt,
+    messages,
+    tools = [],
+    temperature = 0.1,
+    systemPrompt,
 }: {
-  messages: AIMessage[]
-  tools?: any[]
-  temperature?: number
-  systemPrompt?: string
+    messages: AIMessage[]
+    tools?: any[]
+    temperature?: number
+    systemPrompt?: string
 }) => {
-  const formattedTools = tools.map(zodFunction)
+    const formattedTools = tools.map(zodFunction)
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
-    temperature,
-    messages: [
-      {
-        role: 'system',
-        content: systemPrompt || defaultSystemPrompt,
-      },
-      ...messages,
-    ],
-    ...(formattedTools.length > 0 && {
-      tools: formattedTools,
-      tool_choice: 'auto',
-      parallel_tool_calls: false,
-    }),
-  })
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        temperature,
+        messages: [
+            {
+                role: 'system',
+                content: systemPrompt || defaultSystemPrompt,
+            },
+            ...messages,
+        ],
+        ...(formattedTools.length > 0 && {
+            tools: formattedTools,
+            tool_choice: 'auto',
+            parallel_tool_calls: false,
+        }),
+    })
 
-  return response.choices[0].message
+    return response.choices[0].message
+}
+
+export const runApprovalCheck = async (userMessage: string) => {
+    const result = await openai.beta.chat.completions.parse({
+        model: 'gpt-4o-mini',
+        temperature: 0.1,
+        response_format: zodResponseFormat(
+            z.object({
+                approved: z.boolean().describe('did the user approve the action or not')
+            }),
+            'approval'
+        ),
+        messages: [
+            { role: 'system', content: 'Determine if the user approved the image generation. If you are not sure, then it is not approved.' },
+            { role: 'user', content: userMessage }
+        ],
+    })
+
+    return result.choices[0].message.parsed?.approved
 }
